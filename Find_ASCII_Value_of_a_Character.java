eb8f7d1372b3dc6ebb4d6cb0be9e6030dc159230
Devika <97424635+tummala2424@users.noreply.github.com>
2022-09-13 21:00:56 +0530

    import java.util.Scanner; class devika {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         char s;         s=sc.nextLine().charAt(0);         System.out.println((int)s);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class devika
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char s;
+        s=sc.nextLine().charAt(0);
+        System.out.println((int)s);
+    }
+}
