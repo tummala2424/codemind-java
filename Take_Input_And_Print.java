ede5616c3e2cb2726418f316305016a92883306b
Devika <97424635+tummala2424@users.noreply.github.com>
2022-09-13 22:46:09 +0530

    import java.util.Scanner; class input {     public static void main(String args[])     {         String s;         Scanner sc=new Scanner(System.in);         s=sc.nextLine();         System.out.println(s);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class input
+{
+    public static void main(String args[])
+    {
+        String s;
+        Scanner sc=new Scanner(System.in);
+        s=sc.nextLine();
+        System.out.println(s);
+    }
+}
