282c6b20a3f4c3577e6470fe0b8089b9944e8bcc
Devika <97424635+tummala2424@users.noreply.github.com>
2022-09-23 21:35:01 +0530

    import java.util.Scanner; class pattern {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         for(int i=1;i<=n;i++)         {             for(int j=1;j<=n;j++)             {                 if(j==1||j==i||i==n)                     System.out.print("*");                 else                     System.out.print(" ");             }             System.out.println();         }         sc.close();     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -6,11 +6,14 @@ class pattern
         Scanner sc=new Scanner(System.in);
         int n;
         n=sc.nextInt();
-        for(int i=n;i>=1;i--)
+        for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=i;j++)
+            for(int j=1;j<=n;j++)
             {
-                System.out.print(j);
+                if(j==1||j==i||i==n)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
             }
             System.out.println();
         }
