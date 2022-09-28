2bc77f7baacee6f75c9402d78992b24b19e3126c
Devika <97424635+tummala2424@users.noreply.github.com>
2022-09-28 22:43:37 +0530

    import java.util.Scanner; class number {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int p,t,r,si;         p=sc.nextInt();         t=sc.nextInt();         r=sc.nextInt();         si=(p*t*r)/100;         System.out.println(si);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class number
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int p,t,r,si;
+        p=sc.nextInt();
+        t=sc.nextInt();
+        r=sc.nextInt();
+        si=(p*t*r)/100;
+        System.out.println(si);
+    }
+}
