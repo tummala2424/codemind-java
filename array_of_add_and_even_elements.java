3e96d905ef2d285a0174757bde58a972fd06dbe1
Devika <97424635+tummala2424@users.noreply.github.com>
2022-10-06 16:56:41 +0530

    import java.util.Scanner; class number {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,arr[];         n=sc.nextInt();         arr=new int[100];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(arr[i]%2!=0)             {                 System.out.print(arr[i]+" ");             }         }         for(i=0;i<n;i++)         {         if(arr[i]%2==0)         {             System.out.print(arr[i]+" ");         }         }     } }

---
+++ array_of_add_and_even_elements.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class number
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,arr[];
+        n=sc.nextInt();
+        arr=new int[100];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(arr[i]%2!=0)
+            {
+                System.out.print(arr[i]+" ");
+            }
+        }
+        for(i=0;i<n;i++)
+        {
+        if(arr[i]%2==0)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        }
+    }
+}
