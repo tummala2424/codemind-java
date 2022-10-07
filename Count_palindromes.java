14c147d98ef727d554fc234428b27b10f7d229e1
Devika <97424635+tummala2424@users.noreply.github.com>
2022-10-07 17:59:01 +0530

    import java.util.Scanner; class pal {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,arr[],i,d,k=0,temp,c=0;         n=sc.nextInt();         arr=new int[100];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             temp=arr[i];             k=0;             while(arr[i]!=0)             {                 d=arr[i]%10;                 arr[i]=arr[i]/10;                 k=k*10+d;             }             if(k==temp)             {                 c+=1;             }         }         System.out.print(c);     } }

---
+++ Count_palindromes.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class pal
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,arr[],i,d,k=0,temp,c=0;
+        n=sc.nextInt();
+        arr=new int[100];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            temp=arr[i];
+            k=0;
+            while(arr[i]!=0)
+            {
+                d=arr[i]%10;
+                arr[i]=arr[i]/10;
+                k=k*10+d;
+            }
+            if(k==temp)
+            {
+                c+=1;
+            }
+        }
+        System.out.print(c);
+    }
+}
