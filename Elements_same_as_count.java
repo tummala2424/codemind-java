b0fd65aec931e766a9ca6be9707c06aca9a5bdb3
Devika <97424635+tummala2424@users.noreply.github.com>
2022-11-28 18:12:33 +0530

    import java.util.Scanner; class classname { public static void main(String args[]) { Scanner sc=new Scanner(System.in); int n,i,k[],a[],j,c=0,h,z=0; n=sc.nextInt(); a=new int[n]; k=new int[n]; for(i=0;i<n;i++) { a[i]=sc.nextInt(); } for(i=0;i<n;i++) { k[i]=a[i]; } for(i=0;i<n;i++) {    for(j=i+1;j<n;j++)     {         if(a[i]==a[j])         {               a[j]=-1;         }     } } for(i=0;i<n;i++) { if(a[i]>=0) { c=0; for(j=0;j<n;j++) {   if(a[i]==k[j]) {  c+=1; } } if(a[i]==c) {     z+=1;     System.out.print(c+" "); } } } if(z==0) {     System.out.println("-1"); } } }

---
+++ Elements_same_as_count.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+class classname
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n,i,k[],a[],j,c=0,h,z=0;
+n=sc.nextInt();
+a=new int[n];
+k=new int[n];
+for(i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+for(i=0;i<n;i++)
+{
+k[i]=a[i];
+}
+for(i=0;i<n;i++)
+{
+   for(j=i+1;j<n;j++)
+    {
+        if(a[i]==a[j])
+        {
+              a[j]=-1;
+        }
+    }
+}
+for(i=0;i<n;i++)
+{
+if(a[i]>=0)
+{
+c=0;
+for(j=0;j<n;j++)
+{
+  if(a[i]==k[j])
+{
+ c+=1;
+}
+}
+if(a[i]==c)
+{
+    z+=1;
+    System.out.print(c+" ");
+}
+}
+}
+if(z==0)
+{
+    System.out.println("-1");
+}
+}
+}
